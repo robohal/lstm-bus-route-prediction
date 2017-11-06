d7add72e7f39a0ab06acf016fcb4718d234c1412
robohal <robotic.halibut@gmail..com>
2017-11-06 10:54:56 -0800

    some cleanup

--- python/stopstat.py
+++ python/stopstat.py
@@ -14,6 +14,7 @@ bus_id = '1170'
 # MetLinkAPIv1StopDeparturesUrl = "https://www.metlink.org.nz/api/v1/StopDepartures/" + stop_code
 MetLinkAPIv1BusStopUrl = "https://www.metlink.org.nz/api/v1/ServiceLocation/" + bus_num
 
+# look for updates to bus data
 def refreshData():
     response = ur.urlopen(MetLinkAPIv1BusStopUrl)
     raw_json = response.read()
@@ -22,9 +23,11 @@ def refreshData():
     return bus_departures
 # print(bus_departures[0])
 
+# get data for first bus in the list returned from the API.
 def getBusData():
     busList = []
     for bus in refreshData()['Services']:
+        # filter for a bus that has already started its route
         if bus['HasStarted'] == False:
             break
         # print(bus)
@@ -32,10 +35,12 @@ def getBusData():
         break
 
 def followBus():
+    # follow the bus to the end of its route
     pingData = getBusData()
     pingTime = (datetime.strptime(pingData['RecordedAtTime'].split("+")[0], '%Y-%m-%dT%H:%M:%S'))
     print(str(pingTime),pingData['VehicleRef'],pingData['VehicleFeature'],pingData['Bearing'],pingData['DelaySeconds'],pingData['Lat'],pingData['Long'],pingData['Direction'],pingData['OriginStopID'])
     with open('bus.csv', 'a', newline='') as csvfile:
+        # write out to the CSV file
         csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow([str(pingTime),pingData['VehicleRef'],pingData['VehicleFeature'],pingData['Bearing'],pingData['DelaySeconds'],pingData['Lat'],pingData['Long'],pingData['Direction']])
     if getBusData()['HasStarted'] == True:
@@ -44,6 +49,7 @@ def followBus():
         direction = pingData['Direction']
         try:
             while getBusData()['HasStarted'] == True:
+                # keep recording the pings until the bus finishes its route
                 pingData = getBusData()
                 lastPing = (datetime.strptime(pingData['RecordedAtTime'].split("+")[0], '%Y-%m-%dT%H:%M:%S'))
                 time.sleep(30)
@@ -61,6 +67,7 @@ def followBus():
         except TypeError:
             print('waiting for a bus...')
 
+# main function that tries every 30 seconds
 while True: 
     try:
         followBus()
